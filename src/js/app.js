353ebe82c12152b11e971c00933f3ad9311b30c8
VaheAA <vahe.abov@gmail.com>
2022-03-16 03:06:59 +0400

    working slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,24 @@
 import glide from '@glidejs/glide';
 import { Controls } from '../../node_modules/@glidejs/glide/dist/glide.modular.esm.js';
 
-const sliders = document.querySelectorAll('.glide');
+const left = document.getElementById('left');
+const right = document.getElementById('right');
 
-for (var i = 0; i < sliders.length; i++) {
-  var slider = new glide(sliders[i], {
-    gap: 15
-  });
+const leftSlider = new glide(left, {
+  type: 'carousel',
+  startAt: 0,
+  perView: 1
+});
+const rightSlider = new glide(right, {
+  type: 'carousel',
+  startAt: 0,
+  perView: 1,
+  rewind: true
+});
 
-  slider.mount();
-}
+leftSlider.mount();
+rightSlider.mount({ Controls });
+
+rightSlider.on('run', (e) => {
+  leftSlider.go(e.direction);
+});
